d8486a778e2ab949d72a0202cb8b292e3c5e5b0e
lee-cha-dev <leecharlesdev@gmail.com>
2023-06-04 04:45:09 -0500

    Plan to restructure and refactor the code. Switching to just a single struct 'NN', and using pointers to pointers. There were memory leaks occuring, as well as too much abstraction due multiple layers of embedded structures. Having a single structures with pointers will be easier to parse, and handle when using free and release.

--- neural_network_lib.c
+++ neural_network_lib.c
@@ -41,13 +41,12 @@ void neuron_create_weights(struct Neuron *n, double min, double max, int size_we
 void neuron_init(struct Neuron *pN, int size_of_weights, double min, double max){
     // ALLOCATE SPACE TO THE POINTERS INSIDE THE STRUCT
     pN->weights = malloc(sizeof(double[size_of_weights]));
-    pN->bias = malloc(sizeof(double));
 
     // CREATE AND SET THE VALUES FOR THE WEIGHTS TO A RANDOM DOUBLE -- RANDOMNESS MAY
     // BE PREDICTABLE ***
     neuron_create_weights(pN, min, max, size_of_weights);
 
-    *pN->bias = get_random_double(&min, &max);
+    pN->bias = get_random_double(&min, &max);
 }
 
 // NEURON FUNCTIONS TO SET INDIVIDUAL VALUES -- MAY BE UNNECESSARY --
@@ -56,14 +55,15 @@ void neuron_set_weight(struct Neuron *pN, int index, double value){
 }
 
 void neuron_set_bias(struct Neuron *pN, double value){
-    *pN->bias = value;
+    pN->bias = value;
 }
 
 // LAYER FUNCTIONS START
 void layer_init(struct Layer *pL, int size_of_layer, double min, double max, int size_of_next_layer){
     // SET LAYER SIZE
-    pL->layer_size = malloc(sizeof(int));
-    *pL->layer_size = size_of_layer;
+    pL->layer_size = size_of_layer;
+
+    pL->next_layer_size = size_of_next_layer;
 
     pL->neurons = malloc(sizeof(struct Neuron[size_of_layer]));
 
@@ -71,18 +71,13 @@ void layer_init(struct Layer *pL, int size_of_layer, double min, double max, int
 
     for (int i = 0; i < size_of_layer; ++i){
         // CREATE NEURON TO BE ADDED TO ARRAY
-        struct Neuron *temp_neuron;
-        temp_neuron = malloc(sizeof(struct Neuron));
+        struct Neuron temp_neuron;
 
         // INITIALIZE NEURON'S WEIGHTS AND BIAS -- PASS SIZE OF NEXT LAYER FOR WEIGHTS
-        neuron_init(temp_neuron, size_of_next_layer, min, max);
+        neuron_init(&temp_neuron, size_of_next_layer, min, max);
 
         // PASS THE NEURON INTO THE ARRAY
-        pL->neurons[i] = *temp_neuron;
-
-        // FREE THE NEURON'S POINTER AND SET TO NULL
-        free(temp_neuron);
-        temp_neuron = NULL;
+        pL->neurons[i] = temp_neuron;
     }
 }
 
@@ -99,44 +94,40 @@ void neural_network_init(struct NeuralNetwork *pNN, int size_of_nn, double min, 
     int current_size = input_layer_size;
     int next_layer_size;
 
+
     for(int i = 0; i < size_of_nn - 1; ++i){
         // CREATE NEW LAYER
-        struct Layer *l = malloc(sizeof(struct Layer));
+        struct Layer l;
 
         // SET THE NEXT LAYER'S SIZE -- THIS IS CURRENTLY NOT WORKING -- RANDOMNESS WILL BE
         // IMPLEMENTED AT A LATER STAGE
         next_layer_size = get_random_int(&min_hlayer_size, &max_hlayer_size);
 
-
         // INIT NEW LAYER
-        layer_init(l, current_size, min, max, next_layer_size);
+        layer_init(&l, current_size, min, max, next_layer_size);
 
         // ASSIGN THE NEW LAYER TO THE NEURAL NETWORK
-        pNN->layers[i] = *l;
+        pNN->layers[i] = l;
 
-        // RELEASE THE TEMP LAYER
-        free(l);
-        l = NULL;
+        // RELEASE THE TEMP LAYER -- THIS WILL NEED TO RELEASE MEM ALL THE WAY
+        // DOWN TO THE NEURON
 
         // UPDATE THE CURRENT SIZE FOR THE NEXT ITERATION/LAYER -- ALLOWS WEIGHTS OF PREVIOUS LAYER
         // TO MATCH THE NEURONS IN THE CURRENT LAYER
         current_size = next_layer_size;
     }
     // CREATE OUTPUT LAYER
-
-    // CREATE NEW LAYER
-    struct Layer *l = malloc(sizeof(struct Layer));
+    struct Layer l;
 
     // INIT NEW LAYER -- SET THE NEXT LAYER TO ONE -- THIS WILL MOST LIKELY BE CHANGED
     // ONCE THE CODE FOR PASSING DATA HAS WRITTEN. ITS MORE OF REPRESENTATION OF THE NEURON RIGHT NOW
-    layer_init(l, output_layer_size, min, max, 1);
+    layer_init(&l, output_layer_size, min, max, 1);
 
     // ASSIGN THE NEW LAYER TO THE NEURAL NETWORK
-    pNN->layers[size_of_nn - 1] = *l;
+    pNN->layers[size_of_nn - 1] = l;
 
     // RELEASE THE TEMP LAYER
-    free(l);
-    l = NULL;
+
 }
 
 // THIS IS NOT A REAL RANDOM NUMBER GENERATOR. IT IS PREDICTABLE, BUT
@@ -158,21 +149,3 @@ double get_random_int(const int *min, const int *max){
     int total = *min + *max;
     return round(total / 2);
 }
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// WITH THE LOOPS IN NEURAL_NETWORK_INIT, I THINK I CAN CALL MALLOC JUST ONCE, AND                                    //
-// THEN REASSIGN THE VALUES TO THE ADDRESS. THEN, ONCE THE FUNCTION IS FINISHED, I COULD                              //
-// FREE UP THE MEMORY.                                                                                                //
-// IT SEEMS REDUNDANT TO CALL MALLOC SOME MANY TIMES WHEN THE SPACE SHOULD BE THE SAME                                //
-//                                                                                                                    //
-// THOUGH THERE WILL MOST LIKELY NEED TO BE A FUNCTION THAT DEALLOCATES FROM NEURONS, LAYERS, AND NEURONS.            //
-// IN THIS LIBRARY'S CURRENT STATE, I AM FAIRLY SURE THAT THERE ARE MEMORY LEAKS DUE TO ONLY RELEASING                //
-// THE LAYER'S POINT, BUT NOT THE NEURONS POINTERS THAT POINT TO THE WEIGHTS;                                         //
-//                                                                                                                    //
-// THIS SHOULD CREATE A MEMORY LEAK -- THIS WILL BE A PRIORITY TO ADDRESS                                             //
-//                                                                                                                    //
-// POSSIBLY CREATE A FUNCTION THAT WILL TAKE IN A LAYER'S POINT AND RELEASE ALL                                       //
-// MEMORY DOWN TO THE NEURON'S WEIGHTS.                                                                               //
-// THIS MIGHT BE SOMETHING COULD BE HANDLED ON A SEPARATE PROCESS (PROCESSES WILL COME LATER).                        //
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- neural_network_lib.h
+++ neural_network_lib.h
@@ -8,18 +8,38 @@
 // STRUCTS
 struct Neuron {
     double *weights;                    // AN ARRAY OF WEIGHTS
-    double *bias;                       // A SINGLE BIASES FOR THE NEURON -- MIGHT CHANGE THIS TO AN ARRAY LATER
+    double bias;                       // A SINGLE BIASES FOR THE NEURON -- MIGHT CHANGE THIS TO AN ARRAY LATER
 };
 
 struct Layer {
     struct Neuron *neurons;             // ARRAY OF NEURONS
-    int *layer_size;                    // SIZE OF THE LAYER
+    int layer_size;                    // SIZE OF THE LAYER
     int number_of_weights_per_neuron;   // NUMBER OF WEIGHTS EACH NEURON HAS
+    int next_layer_size;
 };
 
 struct NeuralNetwork {
     struct Layer *layers;               // ARRAY OF LAYERS - INCLUDES INPUT AND OUTPUT LAYERS
     int number_of_layers;
+};
+
+//******************************************************************//
+// MIGHT BE BETTER TO USE ONLY ONE STRUCT AND HAVE POINTERS TO
+// POINTERS FOR ARRAYS.
+// THIS WOULD ENSURE THAT POINTERS ARE NOT BEING
+// LOST IN OTHER STRUCTURES
+// THIS COULD REMOVE THE LEVELS OF CODE ABSTRACTION THAT WERE
+// OCCURRING WITH USING NEURON AND LAYER STRUCTS.
+// THE PREVIOUS APPROACH WAS CREATING MEMORY LEAKS,
+// AND A SINGULAR STRUCT IS LESS CONVOLUTED.
+//******************************************************************//
+
+struct NN {
+    double **input_layer;
+    double ***hidden_layers;
+    double **output_layer;
+
+    int number_of_hidden_layers;
 };
 
 // THE MATRIX MULTIPLICATION WILL MOST DEFINITELY NEED TO ACCESS MEMORY IN BLOCKS. I WOULD LIKE TO
@@ -28,12 +48,14 @@ struct NeuralNetwork {
 // SETTING/HANDLING THE NEURON'S VALUES
 void neuron_init(struct Neuron *pN, int size_of_weights, double min, double max);
 void neuron_set_neuron_weights(int number_of_weights, struct Neuron *n, const double *values);
+void neuron_free_weights(struct Neuron *pN);
 void neuron_create_weights(struct Neuron *n, double min, double max, int size_weights);
 void neuron_set_weight(struct Neuron *pN, int index, double value);
 void neuron_set_bias(struct Neuron *pN, double value);
 
 // SETTING && HANDLING THE LAYER'S VALUES
 void layer_init(struct Layer *pL, int size_of_layer, double min, double max, int size_of_next_layer);
+void layer_free_neurons(struct Layer *pL);
 
 // SETTINGS && HANDLING THE NEURAL NETWORK'S VALUES
 void neural_network_init(struct NeuralNetwork *pNN, int size_of_nn, double min, double max, int input_layer_size, int output_layer_size, int min_hlayer_size, int max_hlayer_size);
