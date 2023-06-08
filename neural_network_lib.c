faa44a27e8dc07b31c950d1ebe55d8a5044064b5
lee-cha-dev <leecharlesdev@gmail.com>
2023-06-08 12:55:31 -0700

    Added in functionality for hidden layer init. This was added in remotely so it will need to be ran within the test environment prior to making any updates/changes.

--- neural_network_lib.c
+++ neural_network_lib.c
@@ -46,8 +46,33 @@ void nn_init(struct NN *pNN, double min_weight_val, double max_weight_val, int i
     }
 
     // SETTING FIRST HIDDEN LAYER SIZE FOR FREE_NN TESTING
-    pNN->hidden_layer_sizes = malloc(hidden_layer_size * sizeof(int));
-    pNN->hidden_layer_sizes[0] = next_layer_size;
+    pNN->hidden_layer_sizes = malloc(hidden_layer_size * sizeof(int));  // MOVE THIS TO MEM INIT @TOP
+
+    // CODES NEEDS TO BE RAN WITHIN TEST ENV -- STARTS
+    //------------------------------------------------------------------------------------------------------------//
+
+    // CREATE THE HIDDEN LAYERS
+    // ACCESSING INDIVIDUAL HIDDEN LAYERS
+    for(int i = 0; i < hidden_layer_size; ++i){
+        pNN->hidden_layer_sizes[i] = next_layer_size;
+        pNN->hidden_layers[i] = malloc(next_layer_size * sizeof(double**));
+
+        // UPDATE CURRENT LAYER
+        int current_layer = next_layer_size;
+        next_layer_size = (min_hlayer_size + max_hlayer_size) / 2;
+
+        // CREATE NEURONS WITHIN EACH HIDDEN LAYER
+        for(int j = 0; j < current_layer; ++j){
+            pNN->hidden_layers[i][j] = malloc(next_layer_size * sizeof(double));
+
+            // ASSIGN DOUBLE VALUES TO WEIGHT OF EACH NEURON
+            for(int k = 0; k < next_layer_size; ++k){
+                pNN->hidden_layers[i][j][k] = 0.00;
+            }
+        }
+    }
+    //------------------------------------------------------------------------------------------------------------//
+    // CODES NEEDS TO BE RAN WITHIN TEST ENV -- ENDS
 }
 
 void free_nn_pointers(struct NN *pNN, int input_layer_size){
